dec54a0f61760c9df1cac85d6e25be7b8a5d5cf9
Egor <egor.karitskiy@gmail.com>
2020-10-11 22:53:04 +0300

    14.B 14.C

---
+++ 14/14.B.py
@@ -0,0 +1,15 @@
+def insertion_sort(arr):
+    for i in range(1, len(arr)):
+        key = arr[i]
+        j = i - 1
+        while j >= 0 and key < arr[j]:
+            arr[j + 1] = arr[j]
+            j -= 1
+        arr[j + 1] = key
+
+
+if __name__ == '__main__':
+    n = input()
+    input_array = list(map(int, input().split()))
+    insertion_sort(input_array)
+    print(*input_array)
