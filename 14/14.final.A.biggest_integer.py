1da82c3733c82c6a467680b701d77de9487bc024
Egor <egor.karitskiy@gmail.com>
2020-10-14 15:39:24 +0300

    14 final tasks after-review updates

--- 14/14.final.A.biggest_integer.py
+++ 14/14.final.A.biggest_integer.py
@@ -1,29 +1,25 @@
-# Посылка 36124975
+# Посылка 36295621
 
-def lexicographically_bigger(str_x: str, str_y: str):
-    str_xy = str_x + str_y
-    str_yx = str_y + str_x
-    if str_xy > str_yx:
-        return True
-    return False
+class LexicographicalStr(str):
+    def __ge__(self, other):
+        return self + other >= other + self
+
+    def __le__(self, other):
+        return self + other <= other + self
+
+    def __lt__(self, other):
+        return self + other < other + self
+
+    def __gt__(self, other):
+        return self + other > other + self
 
 
 def biggest_integer(array):
-    array = list(map(str, array))
-    for i in range(1, len(array)):
-        key = array[i]
-        j = i - 1
-        while j >= 0 and lexicographically_bigger(key, array[j]):
-            array[j + 1] = array[j]
-            j -= 1
-        array[j + 1] = key
+    array = sorted(array, reverse=True)
     return ''.join(array)
 
 
 if __name__ == '__main__':
-    n = input()
-    try:
-        input_array = list(map(int, input().split()))
-    except EOFError:
-        input_array = []
+    input()
+    input_array = list(map(LexicographicalStr, input().split()))
     print(biggest_integer(input_array))
--- 14/14.final.B.radix_sort.py
+++ 14/14.final.B.radix_sort.py
@@ -1,29 +1,28 @@
-# Посылка 36139019
+# Посылка 36312869
 
 def counting_sort(array, digit_pos):
     array_length = len(array)
     output_array = [0] * array_length
     digits_counter_array = [0] * 10
-    exp = 1
-    while digit_pos > 1:
-        exp *= 10
-        digit_pos -= 1
-    for i in range(0, array_length):
-        base_division = array[i] / exp
-        digit = int(base_division % 10)
+    elements_result_positions_array = [0] * 10
+    exp = 10 ** digit_pos
+    order_index = 0
+
+    for element in array:
+        digit = element // exp % 10
         digits_counter_array[digit] += 1
 
-    for i in range(1, 10):
-        digits_counter_array[i] += digits_counter_array[i - 1]
+    for i in range(0, 9):
+        order_index += digits_counter_array[i]
+        if digits_counter_array[i] > 0:
+            elements_result_positions_array[i] = order_index
 
-    i = array_length - 1
-    while i >= 0:
-        base_division = array[i] / exp
-        digit = int(base_division % 10)
-        output_array_index = digits_counter_array[digit] - 1
-        output_array[output_array_index] = array[i]
-        digits_counter_array[digit] -= 1
-        i -= 1
+    for element in reversed(array):
+        digit = element // exp % 10
+        position_of_element_in_output_array = (
+                elements_result_positions_array[digit] - 1)
+        output_array[position_of_element_in_output_array] = element
+        elements_result_positions_array[digit] -= 1
 
     for i in range(0, len(array)):
         array[i] = output_array[i]
@@ -32,15 +31,12 @@ def counting_sort(array, digit_pos):
 def radix_sort(array):
     max_number = max(array)
     max_number_of_digits = len(str(max_number))
-    for digit_position in range(1, max_number_of_digits + 1):
+    for digit_position in range(0, max_number_of_digits):
         counting_sort(array, digit_position)
 
 
 if __name__ == '__main__':
-    n = input()
-    try:
-        input_array = list(map(int, input().split()))
-    except EOFError:
-        input_array = []
+    input()
+    input_array = list(map(int, input().split()))
     radix_sort(input_array)
     print(*input_array)
